64602942c820545953921205ad05acaa9389cad4
b2blosangeles <b2blosangeles@gmail.com>
2016-08-03 22:50:18 -0700

    Update getuse.js

--- api/getuse.js
+++ api/getuse.js
@@ -1,10 +1,7 @@
 (function () { 
 	var obj =  function (pkg, env, req, res) {
 		this.load = function() {
-			var myFunction = new Function("a", "b", "return a * b");
-
-			var x = myFunction(4, 3);
-			res.send(x);
+			res.send('x');
 		};	
 	};
 	
