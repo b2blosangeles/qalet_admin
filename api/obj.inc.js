e6d6b84bf8bcf8b694993ef4c621c9d47fe79835
b2blosangeles <b2blosangeles@gmail.com>
2016-08-04 08:45:35 -0700

    Create obj.inc.js

---
+++ api/obj.inc.js
@@ -0,0 +1,12 @@
+(function () { 
+	var obj =  function (pkg, env, req, res) {
+		this.load = function() {
+			res.send(new Date().toString());
+		};
+	};
+	
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = obj;
+	}
+	
+})();
