f3d86f3096ee1078a9584418e64718eb84bdbe1d
b2blosangeles <b2blosangeles@gmail.com>
2016-08-04 18:11:22 -0700

    Update getuse.js

--- api/getuse.js
+++ api/getuse.js
@@ -1,5 +1,5 @@
 // res.send(__dirname + '/obj.inc.js');
-delete require.cache[__dirname + '__dirname + '/obj.inc.js];
+delete require.cache[__dirname + '/obj.inc.js'];
 var o = require(__dirname + '/obj.inc.js');
 var v = new o(pkg, env, req, res);
 v.load();
