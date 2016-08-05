631ce5eab4ca6895b57c8a22a510a88e8e087523
b2blosangeles <b2blosangeles@gmail.com>
2016-08-04 18:45:01 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,1 +1,12 @@
-res.send('--test--');
+(function () { 
+	var obj =  function (pkg, env, req, res) {
+		this.call = function() {
+			res.send(new Date().toString() + '==AAA==');
+		};
+	};
+	
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = obj;
+	}
+	
+})();
