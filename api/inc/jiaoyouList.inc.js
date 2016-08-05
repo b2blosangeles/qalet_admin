f52640ce0969fe5d334692e36e25f359089f6a2d
b2blosangeles <b2blosangeles@gmail.com>
2016-08-04 19:19:06 -0700

    Update jiaoyouList.inc.js

--- api/inc/jiaoyouList.inc.js
+++ api/inc/jiaoyouList.inc.js
@@ -1,1 +1,12 @@
-test
+(function () { 
+	var obj =  function (pkg, env, req, res) {
+		this.call = function() {
+			return {id:1};
+		};
+	};
+	
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = obj;
+	}
+	
+})();
