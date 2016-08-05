cc424348b0145313fce941b265b076d15c527e8b
b2blosangeles <b2blosangeles@gmail.com>
2016-08-04 19:33:41 -0700

    Update getuse.js

--- api/getuse.js
+++ api/getuse.js
@@ -2,6 +2,7 @@
 (function () { 
 	var obj =  function (pkg, env, req, res) {
 		this.call = function() {
+			delete require.cache[ __dirname +'/inc/jiaoyouList.inc.js'];
 			var JYclass = require( __dirname +'/inc/jiaoyouList.inc.js');
 			var o = new JYclass();
 			res.send(o.getData());
