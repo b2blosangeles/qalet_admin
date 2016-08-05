5527d989b4ff85533cca7445ffd8a7ef76103d30
b2blosangeles <b2blosangeles@gmail.com>
2016-08-05 06:34:18 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -3,8 +3,7 @@
 			var JYclass = require(env.space_path  + 'inc/jiaoyouList.inc.js');
 			var o = new JYclass();
 			var fs = require('fs');
-			
-			
+	
 			pkg.fs.readFile(env.space_path + 'tpl/page.tpl', 'utf8', 
 				function(err, code) {
 					res.send(code);
