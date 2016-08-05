60c026d6bde288ff84df2d370030377360f5f577
b2blosangeles <b2blosangeles@gmail.com>
2016-08-04 18:11:54 -0700

    Update obj.inc.js

--- api/obj.inc.js
+++ api/obj.inc.js
@@ -1,7 +1,7 @@
 (function () { 
 	var obj =  function (pkg, env, req, res) {
 		this.load = function() {
-			res.send(new Date().toString() + '===');
+			res.send(new Date().toString() + '== * ==');
 		};
 	};
 	
