4751e5a1f9c749b4339f60c15e46235c8d77a9fc
b2blosangeles <b2blosangeles@gmail.com>
2016-08-05 14:29:55 -0700

    test

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ app.controller('topMenuController', function($rootScope, $scope, $location, $coo
 });
 
 app.config(function($routeProvider) {
-  $routeProvider.when('/',              			{templateUrl: 'uiview/home.html', reloadOnSearch: false});
+  $routeProvider.when('/',              {templateUrl: 'uiview/home.html', reloadOnSearch: false});
   $routeProvider.when('/about',			{templateUrl: 'uiview/about.html', reloadOnSearch: false}); 
   $routeProvider.when('/contact',		{templateUrl: 'uiview/contact.html', reloadOnSearch: false}); 
 });
