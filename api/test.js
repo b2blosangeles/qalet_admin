ed6ef0da4e624c0960564c72bd1b6f6456d13cf3
b2blosangeles <b2blosangeles@gmail.com>
2016-08-05 06:24:11 -0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -2,6 +2,7 @@
 			delete require.cache[env.space_path + 'inc/jiaoyouList.inc.js'];
 			var JYclass = require(env.space_path  + 'inc/jiaoyouList.inc.js');
 			var o = new JYclass();
+			var fs = require('fs');
 			res.send(o.getData());
 
 //res.send(env);
