9f2140255fbbe5e3db2ab5aee3dde7daf8bb2a3f
b2blosangeles <b2blosangeles@gmail.com>
2016-08-06 07:46:42 -0700

    test

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,8 @@
-// res.send(new Date().toString() + '==CC==' + dirname);
-var userinfo = {uid: 1, user:'john shu', token:'1212', command:req.body};
-res.send(userinfo); 
-		//	res.send(o.getData());
-
-//res.send(env);
+switch(req.body.opt) {
+    case 'signin':
+		var userinfo = {uid: 1, user:'john shu', token:'1212', command:req.body};
+		res.send(userinfo); 
+        break;
+    default:
+        res.send(true);
+}
