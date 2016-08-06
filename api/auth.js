db951cea7d355bb58faa3361653631f6333d8ddb
b2blosangeles <b2blosangeles@gmail.com>
2016-08-06 07:29:35 -0700

    test

---
+++ api/auth.js
@@ -0,0 +1,6 @@
+// res.send(new Date().toString() + '==CC==' + dirname);
+var userinfo = {uid: 1, user:'john shu', token:'1212'};
+res.send(userinfo); 
+		//	res.send(o.getData());
+
+//res.send(env);
