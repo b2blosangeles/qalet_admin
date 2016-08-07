d6cd52a8c3b7a5739b3d402d293cd3fe883d5cc4
b2blosangeles <b2blosangeles@gmail.com>
2016-08-07 09:12:54 -0700

    test

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ app.controller('authController', function($rootScope, $scope, $location, $http, 
 		  });				
 	}
 	$scope.signup = function() {
-		$rootScope.progress_modal('signup', 'on', 'Sign up ...');
+		$rootScope.progress_modal('signup', 'on', 'Sign up ...', 1000);
 		$timeout(function() {
 			$rootScope.progress_modal('signup', 'off');
 		},10000);
