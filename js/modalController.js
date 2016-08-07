d8781509caae04e39daebec31aa418c311ad4a4f
b2blosangeles <b2blosangeles@gmail.com>
2016-08-07 09:02:16 -0700

    test

--- js/modalController.js
+++ js/modalController.js
@@ -20,10 +20,8 @@ app.controller('modalController', function($rootScope, $scope, $location, $http,
 			if (curv) {
 				$scope.progress_message = '';
 				for (var k in $scope.Q) {
-				//	$scope.progress_message += $scope.Q[k].message;
+					$scope.progress_message += $scope.Q[k].message;
 				}				
-
-				console.log($scope.Q);
 				$('.qalet_loading_progress_bar').modal();
 			} else {
 				$('.qalet_loading_progress_bar').modal('hide');
