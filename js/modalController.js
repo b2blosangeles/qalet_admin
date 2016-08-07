6be583ed80b2f9a2507050489d5f28f70e5a79a2
b2blosangeles <b2blosangeles@gmail.com>
2016-08-07 08:24:18 -0700

    test

--- js/modalController.js
+++ js/modalController.js
@@ -2,7 +2,7 @@
 app.controller('modalController', function($rootScope, $scope, $location, $http, $cookies){ 
 	
 	$scope.Q = {}; // handle multiple to do
-
+/*
 	$scope.$watch(
 		function() {
 			return $rootScope.Q.type;
@@ -20,7 +20,7 @@ app.controller('modalController', function($rootScope, $scope, $location, $http,
 			}
 		}
 	);	
-	
+*/	
 	$scope.$watch(
 		function() {
 			return Object.keys($scope.Q).length;
@@ -47,12 +47,14 @@ app.controller('modalController', function($rootScope, $scope, $location, $http,
 	
 	
 	$rootScope.progress_modal = function(id, code, message, holdtime) {
-		$rootScope.Q = {type:'progress_modal',code:code, message:message, holdtime:holdtime, id:id};
+	//	$rootScope.Q = {type:'progress_modal',code:code, message:message, holdtime:holdtime, id:id};
+		if (code == 'on') $scope.Q[id] = {type:'progress_modal',code:code, message:message, holdtime:holdtime, id:id};
+		else delete $scope.Q[id];
 	}
 	
 	$rootScope.popup = function(code, message) {
 		//$rootScope.Q = {type:'popup_modal',code:code, message:message};
-		$scope.popup($rootScope.Q.code, $rootScope.Q.message);
+		$scope.popup(code, message);
 	}	
 
 });	
