fa32776d64b76810352743f5008ca4a9d8b53cea
b2blosangeles <b2blosangeles@gmail.com>
2016-08-06 22:49:05 -0700

    test

--- js/main.js
+++ js/main.js
@@ -42,54 +42,6 @@ app.controller('mainController', function($rootScope, $scope, $location, $http, 
 	
 });
 
-
-app.controller('modalController', function($rootScope, $scope, $location, $http, $cookies){ 
-	
-	$scope.$watch(
-		function() {
-			return $rootScope.Q.type;
-		},
-		function(curv, prev) {	
-			if (curv) {
-				if (curv == 'progress_modal') {
-					$scope.progress_modal($rootScope.Q.code, $rootScope.Q.message);
-				}
-				if (curv == 'popup') {
-					$scope.pupup($rootScope.Q.code, $rootScope.Q.message);
-				}	
-				$rootScope.Q = {};
-			}
-		}
-	);	
-
-	$scope.progress_modal = function(code, message) {
-		$scope.progress_message = message;	
-		if (code == 'on') {
-			$('.qalet_loading_progress_bar').modal();
-		} else {
-			$('.qalet_loading_progress_bar').modal('hide');
-		}
-	}
-	
-	$scope.popup = function(code, message) {
-		$scope.popup_message = message;
-		if (code == 'on') {
-			$('.qalet_popup').modal();
-		} else {
-			$('.qalet_popup').modal('hide');
-		}
-	}	
-	
-	$rootScope.progress_modal = function(code, message) {
-		$rootScope.Q = {type:'progress_modal',code:code, message:message};
-	}
-	
-	$rootScope.popup = function(code, message) {
-		$rootScope.Q = {type:'popup',code:code, message:message};
-	}	
-
-});	
-
 app.controller('authController', function($rootScope, $scope, $location, $http, $cookies,  $timeout){ 
 	
 	$scope.signin = function() {
